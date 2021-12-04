ae59764d4d5b0e7253578f6a3232f805d100f1eb
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-12-04 17:13:19 -0600

    Updated AlumnosController.js

--- controllers/AlumnosController.js
+++ controllers/AlumnosController.js
@@ -1,33 +1,32 @@
 // All that is related with Alumnos
 const db = require("../config/db");
+const jwt = require("jsonwebtoken");
 
-exports.loginID = (req,res)=>{
-    const {exp, nip} = req.body;
-    if(exp && nip){
-        let query = "SELECT id FROM alumnos WHERE " 
-        query += `expediente = '${exp}' AND nip = '${nip}';`;
-        const rows = db.query(query);
+exports.loginID = async (req, res, next) => {
+  const { exp, nip } = req.body;
 
-        if(rows.affectedRows == 1){
-            return res.send(JSON.stringify({
-                status: 200, 
-                data: {
-                    msg: rows
-                }
-            }))
-        } else {
-            return res.send(JSON.stringify({
-                status: 500, 
-                data: {
-                    msg: "No se encontró ningun usuario"
-                }
-            }))
-        }
+  const query = "SELECT id FROM alumnos WHERE ";
+  query += `expediente = '${exp}' AND nip = '${nip}';`;
+  const rows = await db.query(query);
+
+  if (exp && nip) {
+    if (rows.length == 1) {
+      const token = jwt.sign(
+        {
+          id: rows[0].id,
+          exp_alumno: rows[0].exp_alumno,
+        },
+        "debugkey"
+      );
+      return res.status(200).json({ code: 200, message: token });
+    } else {
+      return res
+        .status(200)
+        .json({
+          code: 200,
+          message: "Expediente o NIP incorrecto. Intente de nuevo.",
+        });
     }
-    return res.send(JSON.stringify({
-        status: 500, 
-        data: {
-            msg: "Campos Incompletos"
-        }
-    }))
-}
+  }
+  return res.status(500).json({ code: 500, message: "Campos incompletos!" });
+};
