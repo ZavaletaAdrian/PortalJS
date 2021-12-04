f1f5cd170643505a5b4d756091204360efc45e9f
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-12-04 17:16:57 -0600

    Added loginTrabajador route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const AlumnosControllers = require('../controllers/AlumnosController')
+const trabajadorController = require('../controllers/trabajadorController')
 
 module.exports = function(){
     //post /login -- Input: EXP+NIP -- Output: IDUsuario
-    router.post('/login',
+    router.post('/loginAlumno',
         AlumnosControllers.loginID
+    )
+    //Post /login -- Input: NumTrabajador+NIP -- Output: IDTrabajador
+    router.post('/loginTrabajador',
+        trabajadorController.loginID
     )
     return router
 }
