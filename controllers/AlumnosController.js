b87d0a0c4365f8feff17094e49e7b8708e71ffcf
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-12-05 17:26:34 -0600

    Updated controllers

--- controllers/AlumnosController.js
+++ controllers/AlumnosController.js
@@ -1,15 +1,38 @@
 // All that is related with Alumnos
 const db = require("../config/db");
 const jwt = require("jsonwebtoken");
+const Alumnos = require('../models/Alumnos')
+const Sequelize = require('sequelize')
+
+exports.crearAlumno = async(req,res,next)=> {
+  const { exp, nip } = req.body;
+  await Alumnos.create({
+    exp,
+    nip
+  })
+  .then(()=>{
+    return res.status(500).json({message:'creado exitosamente'}) 
+  })
+  .error(()=>{
+    return res.status(500).json({message:'Alog salio mal'})
+  })
+
+}
+
 
 exports.loginID = async (req, res, next) => {
   const { exp, nip } = req.body;
 
-  const query = "SELECT id FROM alumnos WHERE ";
-  query += `expediente = '${exp}' AND nip = '${nip}';`;
-  const rows = await db.query(query);
+  const alumno = await Alumnos.findOne({where:{expediente:exp}})
 
-  if (exp && nip) {
+  if(alumno.verificarPassword(nip)){
+    return res.status(200).json({  message: "Si eres!" });
+  }else{
+    return res.status(400).json({ message: "No eres!" });
+  }
+
+
+  /*if (exp && nip) {
     if (rows.length == 1) {
       const token = jwt.sign(
         {
@@ -27,20 +50,19 @@ exports.loginID = async (req, res, next) => {
           message: "Expediente o NIP incorrecto. Intente de nuevo.",
         });
     }
-  }
-  return res.status(500).json({ code: 500, message: "Campos incompletos!" });
+  }*/
 };
 
 exports.alumnoInfo = async (req, res, next) => {
   const { id } = req.body;
 
-  var query = `SELECT a.expediente, a.IMSS, a.CURP, pe.nombre, pe.duracion, pe.inscripcionesPermitidas, pe.totalMaterias, pe.totalCreditos, a.periodoActivo, a.tipoPeriodo, a.reinscrito, a.vectorInscripcion, `;
-  query += `a.vectorInscripcionEgresado, a.semestre, a.grupo, a.totalNas, a.maximoNas, a.promedio, a.creditosObtenidos, a.materiasAprobadas,`;
-  query += `a.fechaIngreso, c.nombre, c.clave, i.nombre, i.clave, a.activo FROM alumnos a, carreras c, institucions i, planestudios pe WHERE a.id = ${id} AND `;
-  query += `a.carreraId = c.id AND a.institucionId = i.id AND a.planEstudioId = pe.id`
-  const rows = await db.query(query);
+  // var query = `SELECT a.expediente, a.IMSS, a.CURP, pe.nombre, pe.duracion, pe.inscripcionesPermitidas, pe.totalMaterias, pe.totalCreditos, a.periodoActivo, a.tipoPeriodo, a.reinscrito, a.vectorInscripcion, `;
+  // query += `a.vectorInscripcionEgresado, a.semestre, a.grupo, a.totalNas, a.maximoNas, a.promedio, a.creditosObtenidos, a.materiasAprobadas,`;
+  // query += `a.fechaIngreso, c.nombre, c.clave, i.nombre, i.clave, a.activo FROM alumnos a, carreras c, institucions i, planestudios pe WHERE a.id = ${id} AND `;
+  // query += `a.carreraId = c.id AND a.institucionId = i.id AND a.planEstudioId = pe.id`
+  // const rows = await db.query(query);
   console.log(rows.length)
-  return res.status(200).json({ code: 200, message: rows[0] });//"sisoy " + rows[0]
+  return res.status(200).json({ code: 200, message: rows[0] });
 
 
 }
