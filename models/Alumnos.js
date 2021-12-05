dea0fb43011658da444ce2c0ba63f55d58f85a3a
Bruno Paz <brunopaz00@yahoo.com.mx>
2021-12-05 17:37:00 -0600

    modelo alumno corregido

--- models/Alumnos.js
+++ models/Alumnos.js
@@ -19,25 +19,26 @@ const Alumnos = db.define('alumnos',{
             }
         }
     },
-    imss: Sequelize.STRING,
-    curp: Sequelize.STRING,
-    periodoActivo: Sequelize.INTEGER,
-    tipoPeriodo: Sequelize.STRING,
-    reinscrito: Sequelize.STRING,
-    vectorInscripcion: Sequelize.INTEGER,
-    vectorInscripcionEgresado: Sequelize.INTEGER,
-    semestre: Sequelize.INTEGER,
-    grupo: Sequelize.INTEGER,
-    totalNas: Sequelize.INTEGER,
-    maximoNas: Sequelize.INTEGER,
-    promedio: Sequelize.FLOAT,
-    creditosObtenidos: Sequelize.INTEGER,
-    materiasAprobadas: Sequelize.INTEGER,
-    fechaIngreso: Sequelize.DATEONLY,
+    imss: {type: Sequelize.STRING,allowNull: true},
+    curp: {type: Sequelize.STRING,allowNull: true},
+    periodoActivo: {type: Sequelize.INTEGER,allowNull: true},
+    tipoPeriodo: {type: Sequelize.STRING,allowNull: true},
+    reinscrito: {type: Sequelize.STRING,allowNull: true},
+    vectorInscripcion: {type: Sequelize.INTEGER,allowNull: true},
+    vectorInscripcionEgresado: {type: Sequelize.INTEGER,allowNull: true},
+    semestre: {type:Sequelize.INTEGER,allowNull: true},
+    grupo: {type: Sequelize.INTEGER,allowNull: true},
+    totalNas: {type: Sequelize.INTEGER,allowNull: true},
+    maximoNas: {type: Sequelize.INTEGER,allowNull: true},
+    promedio: {type: Sequelize.FLOAT,allowNull: true},
+    creditosObtenidos: {type: Sequelize.INTEGER,allowNull: true},
+    materiasAprobadas: {type: Sequelize.INTEGER,allowNull: true},
+    fechaIngreso: {type: Sequelize.DATEONLY,allowNull: true},
     activo: {
         type:Sequelize.INTEGER,
         // 0 active 1 inactive
-        defaultValue: 0
+        defaultValue: 0,
+        allowNull: true,
     },
 
 })
