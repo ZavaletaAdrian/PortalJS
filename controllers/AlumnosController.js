8f2a8a91f9abdd6bfaa37fa66de304f6a3363731
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-12-05 19:10:32 -0600

    Fixed jwt

--- controllers/AlumnosController.js
+++ controllers/AlumnosController.js
@@ -1,71 +1,110 @@
 // All that is related with Alumnos
 const db = require("../config/db");
 const jwt = require("jsonwebtoken");
-const Alumnos = require('../models/Alumnos')
-const Sequelize = require('sequelize')
-const bcrypt = require('bcrypt-nodejs')
+const Alumnos = require("../models/Alumnos");
+const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt-nodejs");
 
-exports.crearAlumno = async(req,res,next)=> {
+//Models
+const MateriasEnCurso = require("../models/MateriasEnCurso");
+const Profesor = require("../models/Profesor");
+const MateriasPlanEstudios = require("../models/MateriasPlanEstudios");
+const PlanEstudios = require("../models/PlanEstudios");
+const Carrera = require("../models/Carrera");
+const Institucion = require("../models/Institucion");
+const Municipios = require("../models/Municipios");
+const DatosPadres = require("../models/DatosPadres");
+const DatosPersonales = require("../models/DatosPersonales");
+const Paises = require("../models/Paises");
+const Estados = require("../models/Estados");
+
+exports.crearAlumno = async (req, res, next) => {
   const { exp, nip } = req.body;
 
   try {
     await Alumnos.create({
-      expediente:exp,
-      nip
-    })
-    return res.status(200).json({message:'Alumno creado exitosamente'}) 
+      expediente: exp,
+      nip,
+    });
+    return res.status(200).json({ message: "Alumno creado exitosamente" });
   } catch (error) {
-    return res.status(401).json({message: 'El alumno ya existe'})
+    return res.status(401).json({ message: "El alumno ya existe" });
   }
-  
-}
-
+};
 
 exports.loginID = async (req, res, next) => {
   const { exp, nip } = req.body;
 
-  const alumno = await Alumnos.findOne({where:{expediente:exp}})
+  const alumno = await Alumnos.findOne({ where: { expediente: exp } });
 
   // TODO: aqui es donde regresan el token y lo unico que debe tener es el expediente del alumno
-  bcrypt.compare(nip, alumno.nip,(err,valid)=>{
-    if(err)return res.status(400).json({ message: "No eres!" });
-
-    return res.status(200).json({  message: "Si eres!" });
-  })
-
-
-
-  /*if (exp && nip) {
-    if (rows.length == 1) {
-      const token = jwt.sign(
-        {
-          id: rows[0].id,
-          exp_alumno: rows[0].exp_alumno,
-        },
-        "debugkey"
-      );
-      return res.status(200).json({ code: 200, message: token });
-    } else {
+  bcrypt.compare(nip, alumno.nip, (err, valid) => {
+    if (err)
       return res
-        .status(200)
-        .json({
-          code: 200,
-          message: "Expediente o NIP incorrecto. Intente de nuevo.",
-        });
-    }
-  }*/
+        .status(400)
+        .json({ message: "Expediente y/o NIP incorrectos!" });
+
+    const token = jwt.sign(
+      {
+        exp: alumno.exp,
+      },
+      "debugkey"
+    );
+    return res.status(200).json({ message: token });
+  });
 };
 
 exports.alumnoInfo = async (req, res, next) => {
   const { id } = req.body;
 
-  // var query = `SELECT a.expediente, a.IMSS, a.CURP, pe.nombre, pe.duracion, pe.inscripcionesPermitidas, pe.totalMaterias, pe.totalCreditos, a.periodoActivo, a.tipoPeriodo, a.reinscrito, a.vectorInscripcion, `;
-  // query += `a.vectorInscripcionEgresado, a.semestre, a.grupo, a.totalNas, a.maximoNas, a.promedio, a.creditosObtenidos, a.materiasAprobadas,`;
-  // query += `a.fechaIngreso, c.nombre, c.clave, i.nombre, i.clave, a.activo FROM alumnos a, carreras c, institucions i, planestudios pe WHERE a.id = ${id} AND `;
-  // query += `a.carreraId = c.id AND a.institucionId = i.id AND a.planEstudioId = pe.id`
-  // const rows = await db.query(query);
-  console.log(rows.length)
-  return res.status(200).json({ code: 200, message: rows[0] });
-
-
-}
+  const alumno = Alumnos.findOne({
+    where: { expediente: id },
+    include: [
+      //DatosPadres
+      {
+        model: DatosPadres, 
+      },
+      //MateriasCursadas
+      
+      //MateriasEnCurso
+      /*{
+        model: MateriasEnCurso, 
+        include: [
+          Profesor,
+          MateriasPlanEstudios
+        ]  
+      },
+      //PlanEstudios
+      {
+        model: PlanEstudios, 
+        include: [
+          MateriasPlanEstudios
+        ] 
+      },
+      //Carrera 
+      {
+        model: Carrera,
+      },
+      //Institucion
+      {
+        model: Institucion,
+      },
+      //DatosPersonales
+      {
+        model: DatosPersonales,
+        include: [
+          Paises,
+          {
+            model: Municipios,
+            include: [
+              Estados
+            ]
+          },
+        ]
+      },
+      */
+    ]
+  });
+  console.log(alumno);
+  return res.status(200).json({ code: 200, message: alumno });
+};
