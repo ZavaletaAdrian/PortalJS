977e2b082671d7349cc3ebaa04fadcd9bfaeb5f6
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-12-06 20:57:01 -0600

    Fixed data

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ require('./models/Municipios')
 require('./models/Profesor')
 require('./models/MateriasCursadas')
 
+// Middlewares
+const auth = require("./middleware/auth");
+const cors = require("./middleware/cors");
+const index =  require("./middleware/index");
+const notFound = require("./middleware/notFound");
+
 // The models will be created on the db and then the conection to it will ocurr
 db.sync()
     .then(()=> console.log('BD connected'))
@@ -29,19 +35,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/',routes())
 
-// First for production and second for dev
-// const host = process.env.HOST || '0.0.0.0'
-// const port = process.env.PORT || 3000
-// app.listen(port,host,()=>{
-//     console.log('Server listening on port 3000')
-// })
-// app.listen(process.env.PORT || 3000, () =>{
-//     console.log('Server is running...');
-// });
+app.use(notFound);
 
-const PORT = process.env.PORT || 80;
-var server = app.listen(PORT, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("server is listening at http://%s:%s", host, port);
+// First for production and second for dev
+const host = process.env.HOST || '192.168.1.254'
+const port = process.env.PORT || 3000
+app.listen(port,host,()=>{
+    console.log('Server listening on port 3000')
+})
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('Server is running...');
 });
+
+// const PORT = process.env.PORT || 80;
+// var server = app.listen(PORT, function() {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("server is listening at http://%s:%s", host, port);
+// });
