0f58b87e8763210d465ad21ce62cc2764ecbfbc1
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2022-05-24 19:14:56 -0500

    Changed DB connection

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const db = new Sequelize('heroku_6bb141c2c21dca3','b28c71a2f7b3d1','fff6e301',{
-    host:'us-cdbr-east-04.cleardb.com',
-    dialect:'mysql',
-    port:3306,
-    define:{
-        timestamps: false
-    },
+//New conection with Azure DataBase
+const db = new Sequelize("portaldb", "adminjs", "portalJS_1506", {
+  host: "portal-js.mysql.database.azure.com",
+  dialect: "mysql",
+  port: 3306,
+  define: {
+    timestamps: false,
+  },
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-})
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
-module.exports = db
+module.exports = db;
